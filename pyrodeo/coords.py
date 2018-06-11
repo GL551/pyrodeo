aa19a8d6d9082ed7b00908974552c9cc798bf411
Sijme-Jan Paardekooper <s.j.paardekooper@qmul.ac.uk>
2018-06-11 09:13:41 +0100

    Add 3D support

--- pyrodeo/coords.py
+++ pyrodeo/coords.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Coordinate class used in pyrodeo.
 
-The Coordinate class hold the x and y coordinates as 2D ndarrays. In addition, it holds the step size in x and y and the size of the grid in x and y.
+The Coordinate class hold the x, y and z coordinates as 3D ndarrays. In addition, it holds the step size in x, y and z and the size of the grid in x, y and z.
 """
 
 from __future__ import print_function
@@ -14,33 +14,39 @@ class Coordinates(object):
     Args:
         x (ndarray): 2D ndarray containing x coordinates
         y (ndarray): 2D ndarray containing y coordinates
+        z (ndarray): 2D ndarray containing z coordinates
 
     Note:
-        The validity of the arrays is not checked! To be used in a simulation, they should have the same shape, with x[:,i] containing the x coordinates for all i and y[j,:] containing the y coordinates for all j. In addition, both x and y should have a constant step size.
+        The validity of the arrays is not checked! To be used in a simulation, they should have the same shape, with x[:,j,k] containing the x coordinates for all j,k, y[i,:,k] containing the y coordinates for all i,k and z[i,j,:] containing the z coordinates for all i,j. In addition, x, y and z should have a constant step size.
 
     The following public attributes are available:
 
     Attributes:
         x (ndarray): 2D ndarray containing x coordinates
         y (ndarray): 2D ndarray containing y coordinates
-        dimensions ([int, int]): grid dimensions in x and y direction
-        dxy ([float, float]): step size in x and y direction
+        z (ndarray): 3D ndarray containing z coordinates
+        dimensions ([int, int, int]): grid dimensions in the x, y and z direction
+        dxyz ([float, float, float]): step size in the x, y and z direction
 
     """
 
-    def __init__(self, x, y):
-        self.dimensions = (len(x[:,0]), len(y[0,:]))
-        dx = x[1,0] - x[0,0]
+    def __init__(self, x, y, z):
+        self.dimensions = (len(x[:,0,0]), len(y[0,:,0]), len(z[0,0,:]))
+        dx = x[1,0,0] - x[0,0,0]
         dy = dx
-        if len(y[0,:]) > 1:
-            dy = y[0,1] - y[0,0]
-        self.dxy = [dx, dy]
+        dz = dx
+        if len(y[0,:,0]) > 1:
+            dy = y[0,1,0] - y[0,0,0]
+        if len(z[0,0,:]) > 1:
+            dz = z[0,0,1] - z[0,0,0]
+        self.dxyz = [dx, dy, dz]
 
         self.x = x
         self.y = y
+        self.z = z
 
     @classmethod
-    def from_1d(cls, x, y):
+    def from_1d(cls, x, y, z):
         """Initialize from 1D arrays.
 
         Build coordinates from existing 1D ndarrays (should include ghost cells). Calculate dimensions and step sizes.
@@ -48,42 +54,46 @@ class Coordinates(object):
         Args:
             x (ndarray): 1D ndarray containing x coordinates
             y (ndarray): 1D ndarray containing y coordinates
+            z (ndarray): 1D ndarray containing z coordinates
 
         Note:
-            The validity of arrays x and y is not checked. They should have constant step size.
+            The validity of arrays x, y and z is not checked. They should have constant step size.
 
         """
-        x, y = np.meshgrid(x, y, indexing='ij')
-        return cls(x, y)
+        x, y, z = np.meshgrid(x, y, z, indexing='ij')
+        return cls(x, y, z)
 
     @classmethod
     def from_dims(cls,
-                  dimensions=(100, 1),
-                  domain=([-0.5, 0.5], [])):
+                  dimensions=(100, 1, 1),
+                  domain=([-0.5, 0.5], [], [])):
         """Initialize from dimensions and domain size.
 
         Build coordinates given the dimensions of the grid and the size of the domain. Some basic checks are performed to ensure the resulting coordinates are valid.
 
         Args:
-            dimensions (:obj:`(int,int)`, optional): Dimensions of the grid
-            domain (:obj:`([float,float],[float,float])`, optional): Domain boundaries in x and y
+            dimensions (:obj:`(int,int,int)`, optional): Dimensions of the grid
+            domain (:obj:`([float,float],[float,float],[float,float])`, optional): Domain boundaries in x, y and z
         """
         # Check if dimensions valid
-        if len(dimensions) != 2:
-            raise TypeError('Expected dimensions to have two elements')
+        if len(dimensions) != 3:
+            raise TypeError('Expected dimensions to have three elements')
         if (dimensions[1] != 1 and len(domain[1]) != 2):
             raise TypeError('Invalid y domain')
+        if (dimensions[2] != 1 and len(domain[2]) != 2):
+            raise TypeError('Invalid z domain')
         if dimensions[0] <= 1:
             raise ValueError('First grid dimension should be larger than 1')
 
         # Check if domain is valid
-        if len(domain) != 2:
-            raise TypeError('Expected domain to have two elements')
+        if len(domain) != 3:
+            raise TypeError('Expected domain to have three elements')
         if domain[0][1] <= domain[0][0]:
             raise ValueError('Invalid x domain')
 
-        # Step size x and y
+        # Step sizes (all x for now, do y and z later if necessary)
         dxy = [(domain[0][1] - domain[0][0])/np.float(dimensions[0]),
+               (domain[0][1] - domain[0][0])/np.float(dimensions[0]),
                (domain[0][1] - domain[0][0])/np.float(dimensions[0])]
 
         # x coordinates, including two ghost cells on either side
@@ -104,15 +114,30 @@ class Coordinates(object):
         else:
             y = [0.0]
 
-        return cls.from_1d(x, y)
+        # Add z if necessary
+        if dimensions[2] > 1:
+            if domain[2][1] <= domain[2][0]:
+                raise ValueError('Invalid z domain')
+            dxy[2] = (domain[2][1] - domain[2][0])/np.float(dimensions[2])
+            z = np.linspace(domain[2][0] - 1.5*dxy[2],
+                            domain[2][1] + 2.5*dxy[2],
+                            dimensions[2] + 4,
+                            endpoint=False)
+        else:
+            z = [0.0]
+
+        return cls.from_1d(x, y, z)
 
     def __str__(self):
         """Show information about coordinates."""
         return 'Coordinate object, part of pyrodeo. \n \
-    Grid dimensions (nx, ny): {} \n \
+    Grid dimensions (nx, ny, nz): {} \n \
     Domain size x (including ghost zones): {}, {} \n \
-    Domain size y (including ghost zones): {}, {}\n'.format(np.shape(self.x),
+    Domain size y (including ghost zones): {}, {} \n \
+    Domain size z (including ghost zones): {}, {}\n'.format(np.shape(self.x),
                                                             np.min(self.x),
                                                             np.max(self.x),
                                                             np.min(self.y),
-                                                            np.max(self.y))
+                                                            np.max(self.y),
+                                                            np.min(self.z),
+                                                            np.max(self.z))
